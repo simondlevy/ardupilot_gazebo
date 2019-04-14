b80581fc14b537becf051bdcb9a3ce0d9cdccae4
simondlevy <simon.d.levy@gmail.com>
2019-04-14 17:55:44 +0300

    Write log data to file instead of stdout

--- src/ArduPilotPlugin.cc
+++ src/ArduPilotPlugin.cc
@@ -348,26 +348,31 @@ class gazebo::ArduPilotPluginPrivate
     public: int connectionTimeoutMaxCount;
 };
 
+///////////////////////////////////////////////// sdl
+static bool fdmReady;
+static bool airborne;
+static FILE * logfp;
+
+static void report(const char * label, double * x, int n=3)
+{
+    fprintf(logfp, "%s: ", label);
+    for (int k=0; k<n; ++k) {
+        fprintf(logfp, "%+3.3f", x[k]);
+        fprintf(logfp, "%s", (k==n-1) ? " | " : ",");
+    }
+}
+
+
+
 /////////////////////////////////////////////////
 ArduPilotPlugin::ArduPilotPlugin()
     : dataPtr(new ArduPilotPluginPrivate)
 {
     this->dataPtr->arduPilotOnline = false;
     this->dataPtr->connectionTimeoutCount = 0;
-}
 
-/////////////////////////////////////////////////
-static void report(const char * label, double * x, int n=3)
-{
-    printf("%s: ", label);
-    for (int k=0; k<n; ++k) {
-        printf("%+3.3f", x[k]);
-        printf("%s", (k==n-1) ? " | " : ",");
-    }
+    logfp = fopen("log.txt", "w");
 }
-
-static bool fdmReady;
-static bool airborne;
 
 /////////////////////////////////////////////////
 ArduPilotPlugin::~ArduPilotPlugin()
@@ -855,7 +860,7 @@ void ArduPilotPlugin::ReceiveMotorCommand()
     if (fdmReady) {
         float * m = pkt.motorSpeed;
         if (m[0] >= 0.10) airborne = true;
-        if (airborne) printf("m: %2.2f %2.2f %2.2f %2.2f | ", m[0], m[1], m[2], m[3]);
+        if (airborne) fprintf(logfp, "m: %2.2f %2.2f %2.2f %2.2f | ", m[0], m[1], m[2], m[3]);
     }
 
     // Drain the socket in the case we're backed up
@@ -1032,6 +1037,6 @@ void ArduPilotPlugin::SendState() const
     if (airborne) {
         report("g", pkt.imuAngularVelocityRPY);
         report("q", pkt.imuOrientationQuat, 4);
-        printf("\n");
+        fprintf(logfp, "\n");
     }
 }
